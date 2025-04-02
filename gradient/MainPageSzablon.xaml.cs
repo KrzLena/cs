8d93ed7ea3d96a7949b730030a5d10c0d2c35a49
Kciuk <135543040+KrzLena@users.noreply.github.com>
2025-04-02 14:24:03 +0200

    Add files via upload

---
+++ gradient/MainPageSzablon.xaml.cs
@@ -0,0 +1,37 @@
+﻿namespace GradientApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private bool isAnimating // Flaga pozwoli nam sterować animacją, nalezy ustawić wartość początkową
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e) // Funkcja obslugująca wywołanie animacji dla gradientu 
+        {
+            if () return; // Sprawdzamy flage dzięki czemu możemy dodać zatrzymanie animacji
+
+            isAnimating = ; // Ustawiamy flagę aby rozpoczać animacje
+
+            Color[] colors =  // Tablica kolorów potrzebna do utworzenia gradientu i animacji
+            {
+            Colors.Red, // Potrzebujemy kilka/kilkanaście kolorów według uznania
+            };
+
+            while (isAnimating) // Główna pętla animacji
+            {
+                for (int i = 0; i < colors.Length - 1; i++) // pętla w zakresie ilosci kolorów w tablicy
+                {
+
+                    gradientB.GradientStops[0].Color = colors[i]; // Mechanizm ustawiający odpowiednie kolory w gradiencie,
+                                                                  // ustawiamy ich tyle ile jest przejśc w gradiencie,
+                                                                  // dla kolejnych przejsc i uzyskania efektu przewijania
+                                                                  // skorzystamy z operatora modulo w indeksie tablicy
+                    await Task.Delay(500); // robimy pauze dla lepszego efektu
+                }
+            }
+        }
+    }
+
+}
